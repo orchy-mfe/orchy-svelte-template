c268eb9fba36d35dffb4020f3c9d79cc183c87c9
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-15 09:03:40 +0100

    css injection

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './app.css'
 import App from './App.svelte'
 
-import OrchyBaseMfe from '@orchy-mfe/spa-adapter'
-export class SvelteMfe extends OrchyBaseMfe {
+import OrchyMicroFrontend from '@orchy-mfe/spa-adapter'
+export class SvelteMfe extends OrchyMicroFrontend {
   async mount(microFrontendProperties) {
     this.app = new App({
       target: this.getContainer(),
