6c68053d3b8b7e77c0e4d1b0a6bed6070ac32dbf
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-15 09:03:40 +0100

    css injection

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 import {visualizer} from 'rollup-plugin-visualizer'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 const port = process.env.PORT || 5000
 
@@ -9,6 +10,7 @@ export default defineConfig(({mode}) => ({
   plugins: [
     svelte({hot: !process.env.VITEST}),
     visualizer(),
+    cssInjectedByJsPlugin()
   ],
   base: mode === 'development' ? `http://localhost:${port}/` : '/orchy-svelte-template/',
   server: {port, cors: true},
